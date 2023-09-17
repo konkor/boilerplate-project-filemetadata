20d2892a1023b05603f0c04e04ab6404ce7b4c41
konkor <kapa76@gmail.com>
2023-09-17 20:57:50 +0300

    Update API

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var express = require('express');
-var cors = require('cors');
+var multer  = require('multer');
+var cors    = require('cors');
+
 require('dotenv').config()
+
+const storage = multer.diskStorage ({
+  destination: (req, file, cb) => {
+    cb(null, '/tmp');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer ({ storage: storage });
 
 var app = express();
 
@@ -11,7 +24,10 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post ('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  //console.log (req.file);
+  res.json ({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+});
 
 
 const port = process.env.PORT || 3000;
